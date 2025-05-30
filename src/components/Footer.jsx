394134eb02a563201bf3d02246ad8ca851319572
Siyanda Madondile <164426031+Siyanda-Madondile@users.noreply.github.com>
2025-05-31 00:36:11 +0200

    Add files via upload

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer() {
     return (
       <footer className="footer">
-        <p>© 2024 MyListeningPod</p>
+        <p>© 2024 Mypodspace</p>
       </footer>
     );
   }
